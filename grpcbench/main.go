a5a83b20a3f3969de14332e01678249d13cd739e
Andrew Gerrand <adg@golang.org>
2017-01-12 12:31:02 +1100

    grpcbench: fix HTTP/2 benchmark, add HTTP/1.1

--- grpcbench/main.go
+++ grpcbench/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"bytes"
+	"crypto/rand"
 	"crypto/tls"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
 	"os"
-	"strings"
 	"time"
 
 	"golang.org/x/net/context"
@@ -17,18 +19,22 @@ import (
 	"google.golang.org/grpc/examples/helloworld/helloworld"
 )
 
+var (
+	numRuns  = flag.Int("n", 2, "number of calls to make")
+	latency  = flag.Duration("latency", 4*time.Millisecond, "artificial latency to introduce (symmetric)")
+	msgSize  = flag.Int("size", 1<<20, "message size")
+	addr     = flag.String("addr", "localhost:8080", "listen address")
+	useGRPC  = flag.Bool("grpc", true, "use GRPC (fall back is plain HTTP)")
+	useHTTP2 = flag.Bool("http2", true, "use HTTP2")
+	verbose  = flag.Bool("v", false, "verbose output")
+)
+
+const (
+	certFile = "cert.pem"
+	keyFile  = "key.pem"
+)
+
 func main() {
-	var (
-		numRuns = flag.Int("n", 2, "number of calls to make")
-		latency = flag.Duration("latency", 4*time.Millisecond, "artificial latency to introduce (symmetric)")
-		msgSize = flag.Int("size", 1<<20, "message size")
-		addr    = flag.String("addr", "localhost:8080", "listen address")
-		useGRPC = flag.Bool("grpc", true, "use GRPC (fall back is plain HTTP)")
-	)
-	const (
-		certFile = "cert.pem"
-		keyFile  = "key.pem"
-	)
 	flag.Parse()
 
 	ready := make(chan struct{})
@@ -52,30 +58,44 @@ func main() {
 			t.TLSClientConfig = &tls.Config{
 				InsecureSkipVerify: true,
 			}
-			if err := http2.ConfigureTransport(t); err != nil {
-				log.Fatal(err)
+			if *useHTTP2 {
+				if err := http2.ConfigureTransport(t); err != nil {
+					log.Fatal(err)
+				}
 			}
 		}
 
 		ctx := context.Background()
 
-		msg := strings.Repeat(" ", *msgSize)
 		for i := 0; i < *numRuns; i++ {
+			randomBytes := make([]byte, *msgSize)
+			n, err := rand.Read(randomBytes)
+			if err != nil {
+				log.Fatal(err)
+			}
+			if n != *msgSize {
+				log.Fatal("didn't read enough bytes")
+			}
+			msg := string(randomBytes)
+
 			t1 := time.Now()
-			var err error
 			var proto string
 			if *useGRPC {
 				_, err = client.SayHello(ctx, &helloworld.HelloRequest{Name: msg})
 				proto = "GRPC"
 			} else {
 				var resp *http.Response
-				resp, err = http.Post("https://"+*addr, "text/plain", strings.NewReader(msg))
+				resp, err = http.Post("https://"+*addr, "text/plain", bytes.NewReader(randomBytes))
 				proto = "HTTP"
 				if resp != nil {
 					proto = resp.Proto
+					resp.Body.Close()
 				}
 			}
-			fmt.Printf("\n%v\t%v\t%v\n", time.Now().Sub(t1), *latency, proto)
+			if *verbose {
+				fmt.Println()
+			}
+			fmt.Printf("%v\t%v\t%v\n", time.Now().Sub(t1), *latency, proto)
 			if err != nil {
 				log.Fatal(err)
 			}
@@ -101,21 +121,37 @@ func main() {
 	} else {
 		var config tls.Config
 		var err error
-		config.NextProtos = []string{"h2"}
+		if *useHTTP2 {
+			config.NextProtos = []string{"h2"}
+		}
 		config.Certificates = make([]tls.Certificate, 1)
 		config.Certificates[0], err = tls.LoadX509KeyPair(certFile, keyFile)
 		if err != nil {
 			log.Fatal(err)
 		}
-		srv := &http.Server{Addr: *addr, TLSConfig: &config}
+		srv := &http.Server{Addr: *addr, TLSConfig: &config, Handler: http.HandlerFunc(validate)}
 		tlsListener := tls.NewListener(l, &config)
 		log.Fatal(srv.Serve(tlsListener))
+	}
+}
+
+func validate(w http.ResponseWriter, r *http.Request) {
+	b, err := ioutil.ReadAll(r.Body)
+	r.Body.Close()
+	if err != nil {
+		log.Fatalf("validate: %v", err)
+	}
+	if len(b) != *msgSize {
+		log.Fatalf("validate: got %d bytes, want %d", len(b), *msgSize)
 	}
 }
 
 type greeter struct {
 }
 
-func (s greeter) SayHello(context.Context, *helloworld.HelloRequest) (*helloworld.HelloReply, error) {
+func (s greeter) SayHello(ctx context.Context, req *helloworld.HelloRequest) (*helloworld.HelloReply, error) {
+	if len(req.Name) != *msgSize {
+		log.Fatalf("greeter: got %d bytes, want %d", len(req.Name), *msgSize)
+	}
 	return &helloworld.HelloReply{}, nil
 }
