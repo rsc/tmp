7109d770c12550c77daded0ea1997e37bd627f86
Russ Cox <rsc@golang.org>
2024-12-13 10:57:08 -0500

    wasmshell: update to demo new bug

---
+++ wasmshell/go.mod
@@ -0,0 +1,5 @@
+module rsc.io/tmp/wasmshell
+
+go 1.24
+
+require robpike.io/ivy v0.3.7
--- wasmshell/main.go
+++ wasmshell/main.go
@@ -7,18 +7,46 @@
 
 package main
 
-import "syscall/js"
+import (
+	"bytes"
+	_ "log"
+	"strings"
+	"syscall/js"
+
+	"robpike.io/ivy/config"
+	"robpike.io/ivy/exec"
+	"robpike.io/ivy/parse"
+	"robpike.io/ivy/run"
+	"robpike.io/ivy/scan"
+)
 
 func main() {
 	println("Go starting")
+
 	js.Global().Get("window").Set("gocallback", js.FuncOf(func(this js.Value, args []js.Value) any {
-		println(args[0].String())
-		return js.ValueOf(Rot13(args[0].String()))
+		println("Callback", args[0].String())
+		var conf config.Config
+		var out bytes.Buffer
+		conf.SetFormat("")
+		conf.SetMaxBits(1e6)
+		conf.SetMaxDigits(1e4)
+		conf.SetMaxStack(100000)
+		conf.SetOrigin(1)
+		conf.SetPrompt("")
+		conf.SetOutput(&out)
+		conf.SetErrOutput(&out)
+		context := exec.NewContext(&conf)
+		scanner := scan.New(context, "input", strings.NewReader(args[0].String()))
+		parser := parse.NewParser("input", scanner, context)
+		out.Reset()
+		run.Run(parser, context, false)
+		return js.ValueOf(out.String())
 	}))
 	select {}
 }
 
 func Rot13(s string) string {
+	BigStack(100000)
 	b := []byte(s)
 	for i, x := range b {
 		if 'A' <= x && x <= 'M' || 'a' <= x && x <= 'm' {
@@ -28,4 +56,10 @@ func Rot13(s string) string {
 		}
 	}
 	return string(b)
+}
+
+func BigStack(n int) {
+	if n > 0 {
+		BigStack(n - 1)
+	}
 }
